4c78538e0415a3020fc2ffdeacf61c378ea76624
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-09-05 23:00:42 -0400

    Cleaning code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather city="London" />
+        <Weather />
       </header>
     </div>
   );
