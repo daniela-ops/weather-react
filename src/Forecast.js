2fa1587d4ae02cf3c19f28dfc31816b6648a06f9
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-09-06 00:26:14 -0400

    Deleted imgs

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,45 +2,39 @@ import React from "react";
 
 export default function Forecast() {
   return (
-    <div class="row">
-      <div class="col-2">
+    <div className="row">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
       </div>
-      <div class="col-2">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
       </div>
-      <div class="col-2">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
       </div>
-      <div class="col-2">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
       </div>
-      <div class="col-2">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
       </div>
-      <div class="col-2">
+      <div className="col-2">
         <h3>2PM</h3>
-        <img src="#"></img>
         <div>
           <strong>22°</strong>
         </div>
