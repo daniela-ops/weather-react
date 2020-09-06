e9ce7d1c85dba77088aadd62284c733ab24da005
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-09-06 00:17:09 -0400

    added hourly forecast

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Weather />
+        <Forecast />
       </header>
     </div>
   );
---
+++ src/Forecast.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function Forecast() {
+  return (
+    <div class="row">
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+      <div class="col-2">
+        <h3>2PM</h3>
+        <img src="#"></img>
+        <div>
+          <strong>22°</strong>
+        </div>
+      </div>
+    </div>
+  );
+}
